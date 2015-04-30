801b0fcdc9297c01c201c79b82ceee2f5772a2b8
Garrett Simpson <elgaricimo@gmail.com>
2015-04-30 11:09:27 -0700

    Let Board set initial pieces

--- checkers/board.rb
+++ checkers/board.rb
@@ -7,18 +7,51 @@ class Board
   def initialize(setup = true)
     @grid = Array.new(8) {Array.new(8) {nil} }
     @setup = setup
-    self.setup_board if setup
+    setup_board if setup
   end
 
   def show_board
 
   end
 
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def inspect
+    grid.each{ |row| puts row.join('')}
+  end
+
+
 
 
  private
-  def setup_board
+  def []=(pos, piece)
+    x, y = pos
+    @grid[x][y] = piece
+  end
 
+  def place_piece(color, pos)
+    self[pos] = Piece.new(color: color, pos: pos,  board: self)
+  end
+
+  def setup_board
+    self.grid.each_with_index do |row, row_indx|
+      row.each_index do |col_indx|
+        if row_indx < 3 && self.class.dark_square?([row_indx, col_indx])
+          place_piece(:red, [row_indx, col_indx] )
+        elsif row_indx > 4 && self.class.dark_square?([row_indx, col_indx])
+
+          place_piece(:white, [row_indx, col_indx] )
+        end
+      end
+    end
+  end
+
+  def self.dark_square?(pos)
+    row, col = pos
+    (row + col) % 2 != 0
   end
 
 
--- checkers/piece.rb
+++ checkers/piece.rb
@@ -1,10 +1,17 @@
 class Piece
-  attr_reader: :color, :king, :pos
+  attr_reader :color, :king, :pos
+  attr_accessor :display
 
   def initialize(options)
     @color = options[:color]
     @king = false
     @pos = options[:pos]
+    @display = 'P'
+    @board = options[:board]
+  end
+  
+  def inspect
+    @display
   end
 
 end
