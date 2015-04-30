3baa729dc930709b9e09433fac83624077b15461
Garrett Simpson <elgaricimo@gmail.com>
2015-04-30 14:00:57 -0700

    Let piece detect open jumps

--- checkers/piece.rb
+++ checkers/piece.rb
@@ -33,20 +33,7 @@ class Piece
     end
   end
 
-  def on_board?(test_pos)
-    row, col = test_pos
-    row.between?(0, 7) && col.between?(0, 7)
-  end
 
-  def open_slides
-    row, col = self.pos
-    co_ords = move_diffs.map do |diff|
-      diff_row, diff_col = diff
-      [row + diff_row, col + diff_col] 
-    end
-
-    co_ords.select{|co_ord| on_board?(co_ord) && board[co_ord].nil?}
-  end
 
   def perform_slide(input_pos)
     return false unless valid_slide?(input_pos)
@@ -57,6 +44,46 @@ class Piece
     true
   end
 
+private
+  def get_vector(hit_pos)
+    [hit_pos.first - @pos.first, hit_pos.last - @pos.last]
+  end
+
+  def on_board?(test_pos)
+    row, col = test_pos
+    row.between?(0, 7) && col.between?(0, 7)
+  end
+  
+  def open_jumps
+    co_ords = slide_co_ords
+    possible_hits = possible_hit_coords(slide_co_ords) 
+    return [] if possible_hits.empty? 
+
+    possible_lands = possible_landings(possible_hits)
+
+    possible_lands.select do |landing_pos|
+      on_board?(landing_pos) && board[landing_pos].nil?
+    end
+  end
+
+  def open_slides
+    co_ords = slide_co_ords 
+    co_ords.select{|co_ord| on_board?(co_ord) && board[co_ord].nil?}
+  end
+
+  def possible_hit_coords(slide_co_ords)
+    slide_co_ords.reject do |hit_pos|
+      board[hit_pos].nil? || board[hit_pos].color == @color
+    end
+  end
+
+  def possible_landings(possible_hit_coords)
+     possible_hit_coords.map do |hit_pos|
+      vector = get_vector(hit_pos)
+      [hit_pos.first + vector.first, hit_pos.last + vector.last]
+    end
+  end
+
   def should_promote?(current_pos)
     current_row  = current_pos.first
     last_row_indx =  (@color == :white ) ? 0 : 7
@@ -65,6 +92,14 @@ class Piece
       true
     else
       false
+    end
+  end
+
+  def slide_co_ords
+    row, col = self.pos
+    co_ords = move_diffs.map do |diff|
+      diff_row, diff_col = diff
+      [row + diff_row, col + diff_col] 
     end
   end
 
