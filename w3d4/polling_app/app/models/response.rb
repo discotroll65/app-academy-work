d53db7addfbeea491c7708cc8d0a3ec58af05cd0
Garrett Simpson <elgaricimo@gmail.com>
2015-05-07 13:23:44 -0700

    User can't answer own poll

--- w3d4/polling_app/app/models/response.rb
+++ w3d4/polling_app/app/models/response.rb
@@ -1,6 +1,7 @@
 class Response < ActiveRecord::Base
   validates :respondent_id, :answer_choice_id, presence: true
   validate :respondent_has_not_already_answered_question
+  validate :respondent_cannot_answer_own_poll
 
   belongs_to(
     :respondent,
@@ -35,6 +36,12 @@ class Response < ActiveRecord::Base
   def respondent_has_not_already_answered_question
     if sibling_responses.exists?(respondent_id: respondent_id)
       errors[:respondent_id] << "can't answer a question twice."
+    end
+  end
+
+  def respondent_cannot_answer_own_poll
+    if answer_choice.question.poll.author.id == respondent_id
+      errors[:respondent_id] << "can't answer your own poll."
     end
   end
 end
