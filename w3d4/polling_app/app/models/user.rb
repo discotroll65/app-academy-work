b048476bb6f49fa9fa3889d6f6b38ddf235b8496
Garrett Simpson <elgaricimo@gmail.com>
2015-05-07 15:03:34 -0700

    almost done bonus

--- w3d4/polling_app/app/models/user.rb
+++ w3d4/polling_app/app/models/user.rb
@@ -14,4 +14,22 @@ class User < ActiveRecord::Base
     primary_key: :id,
     foreign_key: :respondent_id
   )
+
+  def completed_polls
+    Poll.find_by_sql([<<-SQL, id: id])
+      SELECT
+        polls.title, responses.*
+      FROM
+        polls
+      JOIN
+        questions ON polls.id = questions.poll_id
+      JOIN
+        answer_choices ON questions.id = answer_choices.question_id
+      LEFT OUTER JOIN
+        responses ON responses.answer_choice_id = answer_choices.id
+      WHERE
+        responses.respondent_id = 3 AND responses.id IS NOT NULL
+
+    SQL
+  end
 end
