97e285140931e2d467070b3c33b3ba256e9d28f4
Garrett Simpson <elgaricimo@gmail.com>
2015-05-09 21:29:56 -0700

    Finished blog comments feature

--- w4/blogger/db/schema.rb
+++ w4/blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509232824) do
+ActiveRecord::Schema.define(version: 20150510032113) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20150509232824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.text     "body"
+    t.string   "author_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
