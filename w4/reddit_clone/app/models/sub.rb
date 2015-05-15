569e23d8c117682a564fb290903d6ace416c3ebe
Garrett Simpson <elgaricimo@gmail.com>
2015-05-15 12:00:55 -0700

    add associations to user and sub

--- w4/reddit_clone/app/models/sub.rb
+++ w4/reddit_clone/app/models/sub.rb
@@ -1,2 +1,11 @@
 class Sub < ActiveRecord::Base
+  belongs_to(
+    :moderator,
+    class_name: 'User',
+    foreign_key: :moderator_id,
+    primary_key: :id
+  )
+
+  validates :title, :description, presence: true
+  validates :title, uniquness: true
 end
--- w4/reddit_clone/app/models/user.rb
+++ w4/reddit_clone/app/models/user.rb
@@ -12,6 +12,12 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token!
 
+  has_many(
+    :subs,
+    class_name: 'Sub',
+    foreign_key: :moderator_id,
+    primary_key: :id
+  )
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     user && user.is_password?(password) ? user : nil
