84aca7389c17ada3398f43d2d4ae8275649de830
Garrett Simpson <elgaricimo@gmail.com>
2015-05-14 18:30:38 -0700

    almost got ugly notes

--- w4/d4/music_app/app/helpers/application_helper.rb
+++ w4/d4/music_app/app/helpers/application_helper.rb
@@ -4,9 +4,15 @@ module ApplicationHelper
       <input
         type="hidden"
         name="authenticity_token"
-        value="#{form_authenticity_token}" 
+        value="#{form_authenticity_token}"
       >
-    HTML
+      HTML
     html.html_safe
+  end
+
+  def ugly_lyrics(lyrics)
+    lyrics_array = lyrics.split("\r\n")
+    ugly_lyrics_array = lyrics_array.map{|line| "&#9835; #{h(line)}"  }
+    ugly_lyrics_string = ugly_lyrics_array.join("\r\n")
   end
 end
