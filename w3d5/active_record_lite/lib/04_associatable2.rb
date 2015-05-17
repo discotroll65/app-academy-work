691dfafb961ca58f81329e7e4e299e7eb6daf038
Garrett Simpson <elgaricimo@gmail.com>
2015-05-17 12:18:28 -0700

    refactor ActiveRecord Lite has_one :through

--- w3d5/active_record_lite/lib/04_associatable2.rb
+++ w3d5/active_record_lite/lib/04_associatable2.rb
@@ -9,18 +9,29 @@ module Associatable
 
     end_table = source_name.to_s.camelcase.constantize.table_name
     define_method(name) do
-      end_belong_option = self.send(through_name).class.assoc_options[source_name]
-      results = DBConnection.instance.execute(<<-SQL, self.send(through_name).send(through_belong_option.primary_key) )
+      end_belong_option = self.send(through_name)
+                                .class.assoc_options[source_name]
+
+      end_table = end_belong_option.table_name
+      end_foreign_key = end_belong_option.foreign_key
+      end_primary_key = end_belong_option.primary_key
+
+      through_table = through_belong_option.table_name
+      through_primary_key = through_belong_option.primary_key
+      specific_through_id = self.send(through_name)
+                              .send(through_belong_option.primary_key)
+
+      results = DBConnection.instance.execute(<<-SQL, specific_through_id )
         SELECT
-          #{end_belong_option.table_name}.*
+          #{end_table}.*
         FROM
-          #{through_belong_option.table_name}
+          #{through_table}
         JOIN
-          #{end_belong_option.table_name}
+          #{end_table}
         ON
-          #{through_belong_option.table_name}.#{end_belong_option.foreign_key} = #{end_belong_option.table_name}.#{end_belong_option.primary_key}
+          #{through_table}.#{end_foreign_key} = #{end_table}.#{end_primary_key}
         WHERE
-        #{through_belong_option.table_name}.#{through_belong_option.primary_key} = ?
+        #{through_table}.#{through_primary_key} = ?
       SQL
       end_belong_option.model_class.new(results.first)
     end
