ae2fc6beb8d0e99168147f5342c6f1180e3967dc
Garrett Simpson <elgaricimo@gmail.com>
2015-05-17 22:50:07 -0700

    Finish bootstrap phase 4; Static pages setup

--- w5/bootstrap_demo/config/routes.rb
+++ w5/bootstrap_demo/config/routes.rb
@@ -1,4 +1,10 @@
 Links::Application.routes.draw do
+  get '/home' => 'static_pages#home'
+
+  get '/about' => 'static_pages#about'
+
+  get '/contact' => 'static_pages#contact'
+
   root "links#index"
   resource :session
   resources :users
