025befcaa44bd546713c9b46275644f6d2fb9c47
Garrett Simpson <elgaricimo@gmail.com>
2015-05-20 12:04:24 -0700

    finish basic rails lite

--- w5/rails_lite/lib/phase2/controller_base.rb
+++ w5/rails_lite/lib/phase2/controller_base.rb
@@ -16,7 +16,7 @@ module Phase2
 
     # Set the response status code and header
     def redirect_to(url)
-      raise "Already built resonse!" if @already_built_response
+      raise "Already built response!" if @already_built_response
       res["location"] = url
       res.status = 302
       @already_built_response = true
@@ -26,7 +26,7 @@ module Phase2
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
-      raise "Already built resonse!" if @already_built_response
+      raise "Already built response!" if @already_built_response
       @res.content_type = content_type
       @res.body = content
       @already_built_response = true
